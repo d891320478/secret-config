deed6d173a23c5e06b52c77f54756f3dda6bfc5a
htdong <dht31261@gmail.com>
2025-10-23 20:18:18 +0800

    format

--- secret-config-web/src/main/java/com/upely/secretconfig/constants/Constants.java
+++ secret-config-web/src/main/java/com/upely/secretconfig/constants/Constants.java
@@ -1,12 +1,15 @@
 package com.upely.secretconfig.constants;
 
+import java.util.Optional;
+
 /**
  * @author dht31261
  * @date 2025年10月12日 20:13:05
  */
 public class Constants {
 
-    public static final String BASE_PATH = System.getProperty("config.root.path", "/data/config");
+    public static final String BASE_PATH =
+        Optional.ofNullable(System.getenv("CONFIG_ROOT_PATH")).orElse("/data/config");
     public static final String RTK_FILE = BASE_PATH + "/rtk";
     public static final String RTS_FILE = BASE_PATH + "/rts";
 }
